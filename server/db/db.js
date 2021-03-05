c174056830d6365cdda08780360d4e545ea9df9e
Kush Patel <kushpatel21@gmail.com>
2021-03-05 00:32:33 -0600

    fix db

--- server/db/db.js
+++ server/db/db.js
@@ -3,10 +3,33 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
+let config
+
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    operatorsAliases: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+} else {
+  config = {
+    logging: false,
+    operatorsAliases: false,
+  }
+}
+
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false
+    logging: false,
   }
 )
 module.exports = db
